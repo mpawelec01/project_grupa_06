f9c617abacc3e139470b36a8ff3e4f160de896d8
mpawelec01 <55913406+mpawelec01@users.noreply.github.com>
2023-07-06 14:22:11 +0000

    Deploying to gh-pages from @ mpawelec01/project_grupa_06@d51c5082bae9840dc484b0c52057ba2278c0f4aa 🚀

---
+++ index.5d823c22.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[ice-cream-open]"),closeModalBtn:document.querySelector("[ice-cream-close]"),modal:document.querySelector("[ice-cream]")};function c(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",c),e.closeModalBtn.addEventListener("click",c)})();
+//# sourceMappingURL=index.5d823c22.js.map
