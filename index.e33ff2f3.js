fecd80a952b7955c464a6b7f9110ae9f97215b57
mpawelec01 <55913406+mpawelec01@users.noreply.github.com>
2023-07-08 14:22:44 +0000

    Deploying to gh-pages from @ mpawelec01/project_grupa_06@92c1251da1da23ff04a6414ed36109f6b65d3cb9 🚀

---
+++ index.e33ff2f3.js
@@ -0,0 +1,2 @@
+const swiper=new Swiper(".swiper",{loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
+//# sourceMappingURL=index.e33ff2f3.js.map
