7e933ffba755f77369e1b766d7b06b738053568e
CiiSu <131472893+CiiSu@users.noreply.github.com>
2023-07-10 18:41:32 +0000

    Deploying to gh-pages from @ mpawelec01/project_grupa_06@0ff77ec1c6a70ad92a25d2b0e9853f88ca3785b0 🚀

---
+++ index.ab85b577.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[franchise-open]"),closeModalBtn:document.querySelector("[franchise-close]"),modal:document.querySelector("[franchise]")};function n(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",n),e.closeModalBtn.addEventListener("click",n)})();
+//# sourceMappingURL=index.ab85b577.js.map
